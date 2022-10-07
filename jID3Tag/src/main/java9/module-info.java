c27200f5bc5d955d843a76ee48da72266e26766c
mscg82 <giuseppemiscione@gmail.com>
2022-10-07 18:45:19 +0200

    Adding support for jpms

---
+++ jID3Tag/src/main/java9/module-info.java
@@ -0,0 +1,9 @@
+module com.mscg.jID3tags {
+	exports com.mscg.jID3tags.exception;
+	exports com.mscg.jID3tags.file;
+	exports com.mscg.jID3tags.id3v1;
+	exports com.mscg.jID3tags.id3v2;
+	exports com.mscg.jID3tags.objects;
+	exports com.mscg.jID3tags.objects.frames;
+	exports com.mscg.jID3tags.objects.frames.contents;
+}
